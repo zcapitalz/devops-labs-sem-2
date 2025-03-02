b16c19b2b704eb974b2d2f3372d89fdbe6c960cd
zcapitalz <zadneprovskiywork@gmail.com>
2025-03-02 11:15:30 +0300

    feat: lab2

---
+++ 2/run.sh
@@ -0,0 +1,7 @@
+SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+PROJECT_DIR="$(dirname "$SCRIPT_PATH")"
+REPO_DIR_NAME="auth-tokens"
+
+[ ! -d "${REPO_DIR_NAME}" ] && git clone --depth=1 https://github.com/zcapitalz/auth-tokens
+cp "${PROJECT_DIR}/Dockerfile" "${PROJECT_DIR}/${REPO_DIR_NAME}/Dockerfile"
+REPO_DIR_NAME="${REPO_DIR_NAME}" docker compose -f "${PROJECT_DIR}/docker-compose.yml" up --build
