b36033ac367d6a9bd76a8bfff9d6edacf7fe64c9
zcapitalz <zadneprovskiywork@gmail.com>
2025-04-02 21:29:35 +0300

    fix: lab4: remove redundant system image

--- 4/run.sh
+++ 4/run.sh
@@ -5,6 +5,6 @@ export REPO_DIR_NAME="auth-tokens"
 [ ! -d "${REPO_DIR_NAME}" ] && git clone --depth=1 https://github.com/zcapitalz/auth-tokens
 cp "${PROJECT_DIR}/Dockerfile.build" "${PROJECT_DIR}/${REPO_DIR_NAME}/Dockerfile.build"
 cp "${PROJECT_DIR}/Dockerfile" "${PROJECT_DIR}/${REPO_DIR_NAME}/Dockerfile"
+docker compose -f "${PROJECT_DIR}/docker-compose.yml" --profile build build
 docker compose down -v
-docker compose -f "${PROJECT_DIR}/docker-compose.yml" build
 docker compose -f "${PROJECT_DIR}/docker-compose.yml" up
